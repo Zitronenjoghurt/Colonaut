589b3a6b2464319f8be3efde2fa6eea730c538b1
Zitronenjoghurt <gersdorfdominik@gmail.com>
2023-12-28 19:43:58 +0100

    planetary data display has fixed size

--- src/ui/components/data_list.py
+++ src/ui/components/data_list.py
@@ -1,8 +1,9 @@
 import customtkinter as ctk
 
 class DataList(ctk.CTkFrame):
-    def __init__(self, master, data: list[tuple]):
-        super().__init__(master)
+    def __init__(self, master, data: list[tuple], width=250, height=300):
+        super().__init__(master, width=width, height=height)
+        self.pack_propagate(False)
         self.metrics_label = ctk.CTkLabel(self, anchor='w', font=('Helvetica Neue', 14), justify='left')
         self.data_label = ctk.CTkLabel(self, anchor='w', font=('Helvetica Neue', 14), justify='left')
         self.update_data(data)
@@ -14,5 +15,5 @@ class DataList(ctk.CTkFrame):
         self.metrics_label.configure(text=metrics_text)
         self.data_label.configure(text=data_text)
 
-        self.metrics_label.pack(side='left', fill='y', padx=5, pady=5)
-        self.data_label.pack(side='left', fill='y', padx=5, pady=5)
+        self.metrics_label.pack(side='left', padx=5, pady=5, anchor='n')
+        self.data_label.pack(side='left', padx=5, pady=5, anchor='n')

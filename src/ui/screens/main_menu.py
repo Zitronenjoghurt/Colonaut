17d8c6ef68ee63ffd7733ad873ffbd42076c5064
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-01-04 20:45:55 +0100

    adjust main menu layout

--- src/ui/screens/main_menu.py
+++ src/ui/screens/main_menu.py
@@ -4,20 +4,23 @@ from src.ui.screen import Screen
 class MainMenuScreen(Screen):
     def __init__(self, ui_system):
         super().__init__(ui_system=ui_system)
-        self.title_text = ctk.CTkLabel(master=self, text="COLONAUT", font=('ELNATH', 80))
-        self.title_text.pack(pady=(100, 0))
+        self.title_text = ctk.CTkLabel(master=self, text="COLONAUT", font=('ELNATH', 180))
+        self.title_text.pack(pady=(150, 0))
 
-        self.inspired_by = ctk.CTkLabel(master=self, text="inspired by Seedship", font=('ELNATH', 15))
+        self.inspired_by = ctk.CTkLabel(master=self, text="inspired by Seedship", font=('ELNATH', 45))
         self.inspired_by.pack()
 
-        self.btn_start_game = ctk.CTkButton(master=self, text="Start Game", font=("Geist Mono", 20, "bold"), command=self.start_game)
-        self.btn_start_game.pack(pady=(50, 0))
+        self.btn_start_game = ctk.CTkButton(master=self, text="Start Game", font=("Geist Mono", 60, "bold"), command=self.start_game, height=100, width=400)
+        self.btn_start_game.pack(pady=(100, 0))
 
-        self.btn_options = ctk.CTkButton(master=self, text="Options", font=("Geist Mono", 20, "bold"))
-        self.btn_options.pack(pady=(25, 0))
+        self.btn_options = ctk.CTkButton(master=self, text="Options", font=("Geist Mono", 60, "bold"), height=100, width=400)
+        self.btn_options.pack(pady=(50, 0))
 
-        self.btn_quit = ctk.CTkButton(master=self, text="Quit", font=("Geist Mono", 20, "bold"), command=self.ui_system.on_quit)
-        self.btn_quit.pack(pady=(25, 0))
+        self.btn_quit = ctk.CTkButton(master=self, text="Quit", font=("Geist Mono", 60, "bold"), command=self.ui_system.on_quit, height=100, width=400)
+        self.btn_quit.pack(pady=(50, 0))
+
+        self.inspired_by = ctk.CTkLabel(master=self, text="pre-alpha build", font=('ELNATH', 25))
+        self.inspired_by.pack(pady=(100, 0))
 
     def start_game(self) -> None:
         self.ui_system.switch_screen("planet_view")

0949720cd88012a3562c9c6a28f2ff8abb032ca1
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-01-23 14:53:23 +0100

    add ability to retrieve molecules based on if theyre above or below a certain mass

--- src/molecule_database/molecule_database.py
+++ src/molecule_database/molecule_database.py
@@ -12,7 +12,12 @@ class MoleculeDatabase():
             raise RuntimeError("Tried to initialize multiple instances of MoleculeDatabase.")
         self.molecules_by_name = {}
         self.molecules_by_symbol = {}
+
+        # Will be sorted ascending depending on the molecules mass
+        self.name_to_mass = {}
+
         self._load_molecules()
+        self._sort_by_mass()
 
     def _load_molecules(self) -> None:
         data = file_to_dict(MOLECULES_FILE_PATH)
@@ -25,6 +30,11 @@ class MoleculeDatabase():
                 raise RuntimeError(f"An error occured while initializing molecule database: {e}")
             self.molecules_by_name[molecule_name.lower()] = molecule
             self.molecules_by_symbol[molecule.get_symbol().lower()] = molecule
+            self.name_to_mass[molecule_name.lower()] = molecule.get_atomic_mass().convert("u").get_value()
+
+    def _sort_by_mass(self) -> None:
+        sorted_molecules = sorted(self.name_to_mass.items(), key=lambda molecule: molecule[1])
+        self.name_to_mass = dict(sorted_molecules)
         
     @staticmethod
     def get_instance() -> 'MoleculeDatabase':
@@ -39,3 +49,25 @@ class MoleculeDatabase():
         if name_or_symbol in self.molecules_by_symbol:
             return self.molecules_by_symbol[name_or_symbol]
         raise ValueError(f"Molecule with name or symbol {name_or_symbol} does not exist in current molecule database.")
+    
+    def get_molecules_with_mass_above(self, mass: float) -> list[str]:
+        i = 0
+        names = list(self.name_to_mass.keys())
+        masses = list(self.name_to_mass.values())
+
+        while i < len(masses) and masses[i] <= mass:
+            i+=1
+        
+        return names[i:]
+    
+    def get_molecules_with_mass_below(self, mass: float) -> list[str]:
+        i = 0
+        names = list(self.name_to_mass.keys())
+        masses = list(self.name_to_mass.values())
+
+        while i < len(masses):
+            if masses[i] > mass:
+                break
+            i += 1
+        
+        return names[:i]
